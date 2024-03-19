28798fcb429eba9a0df322ba86b4c07db780b5ea
ITS-DNY <dnyandeepgaonkar23@gmail.com>
2024-03-20 00:19:27 +0530

    backend complete v6

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,7 +6,7 @@ import ListedDonationPage from "../Pages/ListedDonationpage/ListedDonationPage";
 import Donation from "../Pages/Donation/Donation";
 import Login from "../Pages/LoginPage/Login";
 import SignUp from "../Pages/SignUpPage/SignUp";
-import FutureProjects from '../Pages/FutureProjectsPage/FutureProjects'
+import FutureProjects from "../Pages/FutureProjectsPage/FutureProjects";
 import TrusteeRegister from "../Pages/TrusteeRegisterPage/TrusteeRegister";
 import NewsAndUpdatesPage from "../Pages/News and Updates/NewsAndUpdate";
 import AdminEditPage from "../Pages/AdminEditPage/AdminEditPage";
@@ -14,6 +14,7 @@ import Dekstop from "../Components/Server/Dekstop";
 import PROUD_TRUSTEES_PAGE from "../Pages/trusteeMembers/trusteeMembers";
 import AccessDenied from "../ErrorPages/AccessDenied/AccessDenied";
 import PrivateRoutes from "./PrivateRoutes";
+import CertificateFrame from "../Components/Certificate/CertificateFrame";
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
@@ -23,20 +24,14 @@ const ProjectRoutes = () => {
           <Route path="*" element={<PageNotFound />} />
           <Route path="/listedDontionPage" element={<ListedDonationPage />} />
           <Route path="/donation" element={<Donation />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<SignUp />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/register" element={<SignUp />} /> */}
           <Route path="/trusteeRegister" element={<TrusteeRegister />} />
           <Route path="/trusteeMember" element={<PROUD_TRUSTEES_PAGE />} />
-          <Route path="/futureprojects" element={<FutureProjects />} />
+          {/* <Route path="/futureprojects" element={<FutureProjects />} /> */}
           <Route path="/newAndUpdates" element={<NewsAndUpdatesPage />} />
-          <Route
-            path="/edit"
-            element={
-              
-                <AdminEditPage />
-              
-            }
-          />
+          <Route path="/certificate" element={<CertificateFrame />} />
+          <Route path="/edit" element={<AdminEditPage />} />
           <Route path="/dekstop" element={<Dekstop />} />
           <Route path="/access-denied" element={<AccessDenied />} />
         </Routes>
